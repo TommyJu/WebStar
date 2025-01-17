9acd065f7f7a2faf14df8082388f709e5f802337
Tommy Ju <tju1@my.bcit.ca>
2025-01-17 11:47:47 -0800

    Add chatbot and exclude secret

---
+++ Pages/ProcessDetails.cshtml.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebStar.Pages
+{
+    public class ProcessDetailsModel : PageModel
+    {
+        public void OnGet(int id) {
+            var proc = Process.GetProcessById(id);
+            ViewData["MyProcess"] = proc;
+        }
+    }
+}
